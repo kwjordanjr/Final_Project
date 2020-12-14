389c026d19df351320e8b38ff9a29772b2b949c6
Kevin Jordan <9kjordan@gmail.com>
2020-12-14 00:02:44 -0800

    thrid commit

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -31,7 +31,6 @@ export default () => {
     const onSubmitHandler = e => {
         e.preventDefault();
         console.log(account);
-        this.forceUpdate();
         
     }
     useEffect(()=>{
